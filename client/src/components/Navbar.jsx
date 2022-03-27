2f931c6d5ec6e1fdbed5b37b0eb8cd442ae4a77d
vuruxy <kaunghtetoo07@gmail.com>
2022-03-27 18:43:50 +0800

    initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//import { HiMenuAlt4 } from "react-icons/hi";
+//import { AiOutlineClone } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+const Navbar = () => {
+	return (
+		<nav className=""></nav>);
+}
+
+export default Navbar;
